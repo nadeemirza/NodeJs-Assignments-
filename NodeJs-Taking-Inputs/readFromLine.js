046742909ad5a630f7baa449fcd5fb5d3029796f
Mohd Ashu <mirza099nadeem.com>
2022-09-29 20:01:52 +0530

    Modified

--- NodeJs-Taking-Inputs/readFromLine.js
+++ NodeJs-Taking-Inputs/readFromLine.js
@@ -3,7 +3,7 @@ r1 = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-r1.question("What is your name ", (name) => {
-  console.log("Hello ", name);
+r1.question("What is your name ?", (name) => {
+  console.log("Hello....", name);
   r1.close();
 });
