f48af0696804f3ed0e6315c57b3dba887a6e4110
Mohd Ashu <mirza099nadeem.com>
2022-09-29 19:17:27 +0530

    Assignment-1 added

---
+++ NodeJs-Taking-Inputs/readFromLine.js
@@ -0,0 +1,9 @@
+const readline = require("readline");
+r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+r1.question("What is your name ", (name) => {
+  console.log("Hello ", name);
+  r1.close();
+});
